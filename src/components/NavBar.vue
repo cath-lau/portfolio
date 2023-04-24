f071ffd6a6e0a26b8f926343f44f9f44cf3362fa
cathlau <catherine.lau.michaud@gmail.com>
2023-04-24 11:45:18 +0200

    Responsive navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,61 @@
 <template>
-    <v-app-bar app color="#111111" dark flat class="px-8">
-      <v-btn text @click="scroll('home')">Catherine Lau</v-btn>
-      <v-spacer></v-spacer>
-      <v-btn text @click="scroll('home')">Home</v-btn>
-      <v-btn text @click="scroll('projects')">Projets</v-btn>
-      <v-btn text @click="scroll('about')">A propos</v-btn>
-      <v-btn text @click="scroll('contact')">Contact</v-btn>
-    </v-app-bar>
+    <div>
+        <v-app-bar color="#111111" dark flat class="px-8">
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="d-md-none"></v-app-bar-nav-icon>
+            <v-btn text @click="scroll('home')" class="d-none d-md-block">Catherine Lau</v-btn>
+            <v-spacer></v-spacer>
+            <div class="d-none d-md-block">
+                <v-btn text v-for="item in items" :key="item.value" @click="scroll(item.value)">{{ item.title }}</v-btn>
+            </div>
+        </v-app-bar>
+
+        <v-navigation-drawer v-model="drawer" absolute color="#111111">
+            <v-list nav dense>
+                <v-list-item-group v-model="group" active-class="grey--text text--accent-4">
+                    <v-list-item v-for="item in items" :key="item.value" @click="scroll(item.value)">
+                        <v-list-item-title >{{ item.title }}</v-list-item-title>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
+    </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                drawer: false,
+                group: null,
+                items: [
+                    {
+                        title: 'Accueil',
+                        value: 'home'
+                    },
+                    {
+                        title: 'Projets',
+                        value: 'projects'
+                    },
+                    {
+                        title: 'A propos',
+                        value: 'about'
+                    },
+                    {
+                        title: 'Contact',
+                        value: 'contact'
+                    }
+                ]
+            }
+        },
+        watch: {
+            group () {
+                this.drawer = false
+            },
+        },
         methods: {
             scroll(refName) {
-            const element = document.getElementById(refName);
-            element.scrollIntoView({ behavior: "smooth" });
+                const element = document.getElementById(refName);
+                element.scrollIntoView({ behavior: "smooth" });
             },
         },
     };
